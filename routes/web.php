1da88076951b5e1a552192dde5cae3aa8256f935
jserranos09 <64979937+jserranos09@users.noreply.github.com>
2021-05-04 20:42:54 -0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -27,18 +27,16 @@ Route::get('/', function () {
 
 // resource uses everything in the TaskController so you dont have to call each one.
 Route::resource('/task', 'App\Http\Controllers\TaskController');
-Route::get('/', 'App\Http\Controllers\TaskController@index');
-
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', 'App\Http\Controllers\TaskController@index')->middleware('auth');
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+/*
+// if router recieves "/user" laravel wul go to user controller and invoce the "index" method
+Route::get('/user', [UserController::class, 'index']);
+// everything after post/ is treated as a parameter and gives it a slug name
+Route::get('post/{slug}', [PostController::class, 'show']);
+*/
