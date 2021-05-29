edbd761d77467217b5a050cc03f0f3167c4e7116
jserranos09 <64979937+jserranos09@users.noreply.github.com>
2021-05-29 15:44:12 -0700

    Create User.php

--- Models/User.php
+++ Models/User.php
@@ -3,12 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\CanResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+// looks for the plural version of the model name (User looks for Users table in databse)
 class User extends Authenticatable
 {
+    /**
+     *   Table: users
+     *      id = int
+     *      role_id = int / null
+     *      name = string
+     *      email = string
+     *      avatar = string / default:users/default.png
+     *       ...
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     */
     use HasFactory, Notifiable;
 
     /**
@@ -21,6 +41,7 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+
 
     /**
      * The attributes that should be hidden for arrays.
@@ -40,4 +61,20 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // relationships. Called "tasks" method
+    public function tasks()
+    {
+        // each user has many tasks
+        return $this->hasMany(Task::class);
+        // loads Task Class
+        // okay, im a user model = user
+        // my primary id = id
+        // if someone belongs to me, they would save user_id
+        // lets check the Task class, to see if they have a user_id
+        // lets take my id = 31
+        // lets peform a query as follows
+        // return  Task::where('user_id', '=', 31)->get();
+        //return $this->hasMany('App\Model\Task');
+    }
 }
