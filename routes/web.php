7b476618977dc218da9216b4967afc67071d5bc3
jserranos09 <64979937+jserranos09@users.noreply.github.com>
2021-05-29 15:11:42 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,18 +25,11 @@ Route::get('/', function () {
 */
 
 
+Auth::routes();
+// go to taskController, find index method and exectue whatever is there.
+Route::get('/', 'App\Http\Controllers\TaskController@index')->middleware('auth');
 // resource uses everything in the TaskController so you dont have to call each one.
 Route::resource('/task', 'App\Http\Controllers\TaskController');
-Route::get('/', 'App\Http\Controllers\TaskController@index')->middleware('auth');
+//Route::get('/task/{slug}', 'TaskController@show');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
-/*
-// if router recieves "/user" laravel wul go to user controller and invoce the "index" method
-Route::get('/user', [UserController::class, 'index']);
-// everything after post/ is treated as a parameter and gives it a slug name
-Route::get('post/{slug}', [PostController::class, 'show']);
-*/
+Route::resource('/contact', 'App\Http\Controllers\ContactUsController');
